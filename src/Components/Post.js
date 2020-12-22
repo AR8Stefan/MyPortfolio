0275b31693780dbc622d35cc0206609cac8dbb76
AR8Stefan <stefan.piatkov@gmail.com>
2020-12-22 14:06:44 -0600

    completed single blog post section

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -34,7 +34,7 @@ export default function Post() {
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
           {postData && postData.map((post, index) => (
           <article>
-            <Link to={"/post/" + post.slug.curent} key={post.slug.curent}>
+            <Link to={"/post/" + post.slug.current} key={post.slug.current}>
             <span 
               className="block h-64 relative rounded shadow leading-snug bg-white b-l-8 b-black" 
               key={index}>
